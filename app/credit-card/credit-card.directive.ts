00986a8d35b8361770e5e625d0be52aa19881933
extrahappyperson <ht.uzpharma@gmail.com>
2019-07-03 15:58:46 +0200

    host listener added

--- app/credit-card/credit-card.directive.ts
+++ app/credit-card/credit-card.directive.ts
@@ -1,10 +1,26 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[credit-card]'
 })
 export class CreditCardDirective {
-  constructor(private element: ElementRef) {
-    console.log(this.element);
+ @HostListener('input', ['$event'])
+ 
+ onKeyDown(event:KeyboardEvent) {
+  const input = event.target as HTMLInputElement; 
+  
+  let trimmered = input.value.replace(/\s+/g,''); 
+
+  if(trimmered.length>16) {
+    trimmered.substr(0,16)
+  }
+
+  let numbers = []; 
+  for(let i = 0; i<trimmered.length; i+=4) {
+    numbers.push(trimmered.substr(i, 4))
+  }
+  
+  input.value = numbers.join(' ');
+ 
   }
 }
